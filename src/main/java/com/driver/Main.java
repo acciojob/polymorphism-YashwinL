0ca637ce86cd1408e027ec977d921e9a329e7161
YashwinL <yashwinlakshmipathy@gmail.com>
2023-01-13 11:42:35 +0530

    Made Poly Change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,27 @@ package com.driver;
 
 public class Main {
 
+    static class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(5,5);
+        p.product(5,5,5);
+        p.product(4.4,5.5);
+
+    }
+
+
+
+
 }
